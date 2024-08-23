15e1c7e2b4eb746fe51c42c76370448b884f61aa
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-08-23 10:30:14 +0300

    Sheet 10 (General Hard)

---
+++ Sheet 10 (General Hard)/B. Delete from the Left .cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	string txt_1 , txt_2;
+	cin >> txt_1 >> txt_2;
+
+	int size_1 = txt_1.size();
+	int size_2 = txt_2.size();
+
+	int reverse_1 = size_1 - 1;
+	int reverse_2 = size_2 - 1;
+
+	int result = 0;
+
+	while (reverse_1 >= 0 && reverse_2 >= 0 && txt_1[reverse_1] == txt_2[reverse_2]){
+		reverse_1--;
+		reverse_2--;
+		result++;
+	}
+	
+	cout << size_1 - result + size_2 - result << endl;
+
+}
