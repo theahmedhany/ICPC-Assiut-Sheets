76d46535941d4325296705ca20d95905a6f592ff
Ahmed Hany <150602779+theahmedhany@users.noreply.github.com>
2024-08-25 12:57:40 +0300

    Add files via upload

---
+++ Sheet 10 (General Hard)/J. Help Vasilisa the Wise 2 .cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	int r1, r2, c1, c2, d1, d2;
+	cin >> r1 >> r2 >> c1 >> c2 >> d1 >> d2;
+
+	for (int a = 1; a <= 9; a++) {
+		for (int b = 1; b <= 9; b++) {
+			for (int c = 1; c <= 9; c++) {
+				for (int d = 1; d <= 9; d++) {
+					if (a != b && a != c && a != d && b != c && b != d && c != d) {
+						if (a + b == r1 && a + d == d1 && c + d == r2 && a + c == c1 && b + d == c2 && b + c == d2) {
+							cout << a << " " << b << endl;
+							cout << c << " " << d << endl;
+							return 0;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	cout << -1 << endl;
+
+}
+
