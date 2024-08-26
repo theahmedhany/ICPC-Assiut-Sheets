3cf31f541ef81b2b1f514e1bd2a137c84263580d
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-08-26 11:30:59 +0300

    Sheet 10 (General Hard)

---
+++ Sheet 10 (General Hard)/K. Snacktower .cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	int size;
+	cin >> size;
+
+	int max = size;
+	int frequency[size + 1] = {0};
+
+	for (int i = 0; i < size; i++) {
+		int number;
+		cin >> number;
+
+		frequency[number] = 1;
+
+		while (frequency[max]) {
+			cout << max << " ";
+			max--;
+		}
+
+		cout << endl;
+	}
+
+}
