e96f17f5d9c330c7a63b54a1afc80c35967947d4
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-08-27 11:46:34 +0300

    Sheet 5 (Functions)

--- Sheet 5 (Functions)/B. Print .cpp
+++ Sheet 5 (Functions)/B. Print .cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-void print(int number){
+void print(int number) {
 
-	for(int i = 1; i <= number; i++) {
-		if(i != 1){
+	for (int i = 1; i <= number; i++) {
+		if (i != 1) {
 			cout << " ";
 		}
 
