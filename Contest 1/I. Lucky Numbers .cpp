d2a68fd7d87571991f70756c896a3f4598304637
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-08-31 10:43:44 +0300

    Contest 1

--- Contest 1/I. Lucky Numbers .cpp
+++ Contest 1/I. Lucky Numbers .cpp
@@ -1,21 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
+int main() {
 
-	string number;
-	cin >> number;
+  string number;
+  cin >> number;
 
-	int first_digit = number[0] - '0';
-	int second_digit = number[1] - '0';
+  if (number.length() < 2) {
+    cout << "NO" << endl;
+    return 0;
+  }
 
-	if (second_digit == 0) {
-		cout << "YES";
-	} else if (first_digit % second_digit == 0 || second_digit % first_digit == 0) {
-		cout << "YES";
-	} else {
-		cout << "NO";
-	}
+  int first_digit = number[0] - '0';
+  int second_digit = number[1] - '0';
+
+  if (second_digit == 0) {
+    cout << "YES" << endl;
+  } else if (first_digit % second_digit == 0 || second_digit % first_digit == 0) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
 
 }
