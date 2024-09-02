b4ded522317095b53f4b9b6ee970e3d2bd062454
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-09-02 11:48:41 +0300

    Sheet 1 (Data type - Conditions)

--- Sheet 1 (Data type - Conditions)/R. Age in Days .cpp
+++ Sheet 1 (Data type - Conditions)/R. Age in Days .cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	long long days_number;
-	cin >> days_number;
+  long long days_number;
+  cin >> days_number;
 
-	int year = days_number / 365;
-	int result = days_number - (year * 365);
-	int month = result / 30;
-	int day = result - (month * 30);
+  int year = days_number / 365;
+  int remaining_days = days_number - (year * 365);
+  int month = remaining_days / 30;
+  int day = remaining_days - (month * 30);
 
-	cout << year << " years\n";
-	cout << month << " months\n";
-	cout << day << " days\n";
+  cout << year << " years\n";
+  cout << month << " months\n";
+  cout << day << " days\n";
 
 }
--- Sheet 1 (Data type - Conditions)/U. Float or int .cpp
+++ Sheet 1 (Data type - Conditions)/U. Float or int .cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	float number;
-	cin >> number;
+  float number;
+  cin >> number;
 
-	int integer_number = number;
+  int integer_number = number;
 
-	if (number / integer_number == 1){
-		cout << "int " << integer_number;
-	}else{
-		cout << "float " << integer_number << " " << number - integer_number;
-	}
+  if (number / integer_number == 1) {
+    cout << "int " << integer_number;
+  } else {
+    cout << "float " << integer_number << " " << number - integer_number;
+  }
 
 }
