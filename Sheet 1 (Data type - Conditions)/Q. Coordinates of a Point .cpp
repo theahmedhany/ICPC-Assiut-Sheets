8a6459ba24e72e2a4b47677d81035e1a1f51e7e9
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-09-02 11:42:59 +0300

    Sheet 1 (Data type - Conditions)

--- Sheet 1 (Data type - Conditions)/Q. Coordinates of a Point .cpp
+++ Sheet 1 (Data type - Conditions)/Q. Coordinates of a Point .cpp
@@ -2,24 +2,25 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	float number_1, number_2;
-	cin >> number_1 >> number_2;
+  float number_1, number_2;
+  cin >> number_1 >> number_2;
 
-	if (number_1 > 0 && number_2 > 0)
-		cout << "Q1";
-	else if (number_1 < 0 && number_2 > 0)
-		cout << "Q2";
-	else if (number_1 < 0 && number_2 < 0)
-		cout << "Q3";
-	else if (number_1 > 0 && number_2 < 0)
-		cout << "Q4";
-	else if (number_1 == 0 && number_2 == 0)
-		cout << "Origem";
-	else if ((number_1 > 0 && number_2 == 0) || (number_1 < 0 && number_2 == 0))
-		cout << "Eixo X";
-	else if ((number_1 == 0 && number_2 > 0) || (number_1 == 0 && number_2 < 0))
-		cout << "Eixo Y";
+  if (number_1 > 0 && number_2 > 0) {
+    cout << "Q1" << endl;
+  } else if (number_1 < 0 && number_2 > 0) {
+    cout << "Q2" << endl;
+  } else if (number_1 < 0 && number_2 < 0) {
+    cout << "Q3" << endl;
+  } else if (number_1 > 0 && number_2 < 0) {
+    cout << "Q4" << endl;
+  } else if (number_1 == 0 && number_2 == 0) {
+    cout << "Origem" << endl;
+  } else if ((number_1 > 0 && number_2 == 0) || (number_1 < 0 && number_2 == 0)) {
+    cout << "Eixo X" << endl;
+  } else if ((number_1 == 0 && number_2 > 0) || (number_1 == 0 && number_2 < 0)) {
+    cout << "Eixo Y" << endl;
+  }
 
 }
