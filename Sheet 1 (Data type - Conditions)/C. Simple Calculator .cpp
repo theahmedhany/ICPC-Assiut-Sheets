d5560df1ef77d1edc5cf9e00c096bfe039e0c0fd
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-09-02 11:12:02 +0300

    Sheet 1 (Data type - Conditions)

--- Sheet 1 (Data type - Conditions)/C. Simple Calculator .cpp
+++ Sheet 1 (Data type - Conditions)/C. Simple Calculator .cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	long long number_1, number_2;
-	cin >> number_1 >> number_2;
+  long long number_1, number_2;
+  cin >> number_1 >> number_2;
 
-	cout << number_1 << " + " << number_2 << " = " << number_1 + number_2 << endl;
-	cout << number_1 << " * " << number_2 << " = " << number_1 * number_2 << endl;
-	cout << number_1 << " - " << number_2 << " = " << number_1 - number_2 << endl;
+  cout << number_1 << " + " << number_2 << " = " << number_1 + number_2 << endl;
+  cout << number_1 << " * " << number_2 << " = " << number_1 * number_2 << endl;
+  cout << number_1 << " - " << number_2 << " = " << number_1 - number_2 << endl;
 
 }
--- Sheet 1 (Data type - Conditions)/D. Difference .cpp
+++ Sheet 1 (Data type - Conditions)/D. Difference .cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	long long number_1, number_2, number_3, number_4;
-	cin >> number_1 >> number_2 >> number_3 >> number_4;
+  long long number_1, number_2, number_3, number_4;
+  cin >> number_1 >> number_2 >> number_3 >> number_4;
 
-	long long result = (number_1 * number_2) - (number_3 * number_4);
+  long long result = (number_1 * number_2) - (number_3 * number_4);
 
-	cout << "Difference = " << result << endl;
+  cout << "Difference = " << result << endl;
 
 }
