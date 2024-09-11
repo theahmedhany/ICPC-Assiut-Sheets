89a1c68ed2cfe275fcdfd97bef16a7c038fdc3ad
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-09-11 15:38:29 +0300

    Contest 3

--- Contest 3/C. Choose Elements .cpp
+++ Contest 3/C. Choose Elements .cpp
@@ -2,31 +2,29 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-  long long size , number;
+  long long size, number;
   cin >> size >> number;
 
   long long arry[size];
 
-  for (int i = 0; i < size; i++){
+  for (int i = 0; i < size; i++) {
     cin >> arry[i];
   }
-  
-  sort(arry , arry + size);
-  reverse(arry , arry + size);
+
+  sort(arry, arry + size);
+  reverse(arry, arry + size);
 
   long long sum = 0;
-  for (int i = 0; i < number; i++){
 
-    if (arry[i] < 0){
+  for (int i = 0; i < number; i++) {
+    if (arry[i] < 0) {
       break;
     }
-
     sum += arry[i];
-
   }
-	
-  cout << sum;
+
+  cout << sum << endl;
 
 }
--- Contest 3/D. Counting Elements .cpp
+++ Contest 3/D. Counting Elements .cpp
@@ -2,43 +2,41 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
   int size;
   cin >> size;
 
   int arry[size];
 
-  for (int i = 0; i < size; i++){
+  for (int i = 0; i < size; i++) {
     cin >> arry[i];
   }
-  
-  sort(arry , arry + size);
+
+  sort(arry, arry + size);
 
   int temp = 0;
   int result = 0;
   int counter = 0;
 
-  for (int i = 0; i < size; i++){
-		
-    if (i == 0){
+  for (int i = 0; i < size; i++) {
+    if (i == 0) {
       temp = arry[i];
       result = 1;
-    }else{
-      if (temp == arry[i]){
+    } else {
+      if (temp == arry[i]) {
         result++;
-      }else if (temp + 1 == arry[i]){
+      } else if (temp + 1 == arry[i]) {
         counter += result;
         temp = arry[i];
         result = 1;
-      }else{
+      } else {
         temp = arry[i];
         result = 1;
       }
     }
-    
   }
-  
+
   cout << counter << endl;
 
 }
