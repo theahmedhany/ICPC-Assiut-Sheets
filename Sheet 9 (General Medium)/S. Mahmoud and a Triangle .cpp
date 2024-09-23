7d07ffa962257cb5b01ceaff1e4447e5b0cb1d5e
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-09-23 11:41:25 +0300

    Sheet 9 (General Medium)

--- Sheet 9 (General Medium)/S. Mahmoud and a Triangle .cpp
+++ Sheet 9 (General Medium)/S. Mahmoud and a Triangle .cpp
@@ -2,30 +2,28 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	int size;
-	cin >> size;
+  int size;
+  cin >> size;
 
-	int arry[size];
+  int arry[size];
 
-	for(int i = 0; i < size; i++){
-		cin >> arry[i];
-	}
+  for (int i = 0; i < size; i++) {
+    cin >> arry[i];
+  }
 
-	sort(arry, arry + size);
+  sort(arry, arry + size);
 
-	string result = "NO";
+  string result = "NO";
 
-	for (int i = size - 1; i >= 2; i--) {
-		if (arry[i] + arry[i - 1] > arry[i - 2] 
-					&& arry[i - 1] + arry[i - 2] > arry[i] 
-					&& arry[i] + arry[i - 2] > arry[i - 1]) {
-			result = "YES";
-			break;
-		}
-	}
+  for (int i = size - 1; i >= 2; i--) {
+    if (arry[i] + arry[i - 1] > arry[i - 2] && arry[i - 1] + arry[i - 2] > arry[i] && arry[i] + arry[i - 2] > arry[i - 1]) {
+      result = "YES";
+      break;
+    }
+  }
 
-	cout << result;
+  cout << result << endl;
 
 }
--- Sheet 9 (General Medium)/T. Olesya and Rodion .cpp
+++ Sheet 9 (General Medium)/T. Olesya and Rodion .cpp
@@ -2,27 +2,25 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-  int size , divide;
+  int size, divide;
   cin >> size >> divide;
 
   string result = "";
 
-  if (size < 2 and divide == 10){
+  if (size < 2 && divide == 10) {
     cout << -1 << endl;
-  } else if (size >= 2 and divide == 10) {
-    for (int i = 0; i < size - 1; i++){
+  } else if (size >= 2 && divide == 10) {
+    for (int i = 0; i < size - 1; i++) {
       result += 1 + '0';
     }
-    
     result += 0 + '0';
     cout << result << endl;
   } else {
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
       result += divide + '0';
     }
-
     cout << result << endl;
   }
 
