393a737cd9d6b9b55a820d74caf4f29a474c09fe
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-09-24 16:56:31 +0300

    Sheet 9 (General Medium)

--- Sheet 9 (General Medium)/V. Balanced Rating Changes .cpp
+++ Sheet 9 (General Medium)/V. Balanced Rating Changes .cpp
@@ -2,37 +2,37 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	int size;
-	cin >> size;
+  int size;
+  cin >> size;
 
-	int arry[size];
+  int arry[size];
 
-	for (int i = 0; i < size; i++) {
-		cin >> arry[i];
-	}
+  for (int i = 0; i < size; i++) {
+    cin >> arry[i];
+  }
 
-	bool flag = 0;
+  bool flag = 0;
 
-	for (int i = 0; i < size; i++) {
-		double rate = (double) arry[i] / 2;
+  for (int i = 0; i < size; i++) {
+    double rate = (double) arry[i] / 2;
 
-		if (arry[i] % 2 == 0) {
-			arry[i] = arry[i] / 2;
-		} else {
-			if (flag == 0) {
-				arry[i] = ceil(rate);
-				flag = 1;
-			} else {
-				arry[i] = floor(rate);
-				flag = 0;
-			}
-		}
-	}
+    if (arry[i] % 2 == 0) {
+      arry[i] = arry[i] / 2;
+    } else {
+      if (flag == 0) {
+        arry[i] = ceil(rate);
+        flag = 1;
+      } else {
+        arry[i] = floor(rate);
+        flag = 0;
+      }
+    }
+  }
 
-	for (int i = 0; i < size; i++) {
-		cout << arry[i] << endl; 
-	}
+  for (int i = 0; i < size; i++) {
+    cout << arry[i] << endl;
+  }
 
 }
--- Sheet 9 (General Medium)/X. Mountain Scenery .cpp
+++ Sheet 9 (General Medium)/X. Mountain Scenery .cpp
@@ -2,31 +2,31 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	int size, number;
-	cin >> size >> number;
+  int size, number;
+  cin >> size >> number;
 
-	int points = (size * 2) + 1;
-	int arry[points];
+  int points = (size * 2) + 1;
+  int arry[points];
 
-	for (int i = 0; i < points; i++) {
-		cin >> arry[i];
-	}
+  for (int i = 0; i < points; i++) {
+    cin >> arry[i];
+  }
 
-	for (int i = 1; i < points; i++) {
-		if (arry[i] > arry[i - 1] + 1 && arry[i] > arry[i + 1] + 1) {
-			arry[i]--;
-			number--;
+  for (int i = 1; i < points - 1; i++) {
+    if (arry[i] > arry[i - 1] + 1 && arry[i] > arry[i + 1] + 1) {
+      arry[i]--;
+      number--;
 
-			if (number == 0) {
-				break;
-			}
-		}
-	}
+      if (number == 0) {
+        break;
+      }
+    }
+  }
 
-	for (int i = 0; i < points; i++) {
-		cout << arry[i] << " ";
-	}
+  for (int i = 0; i < points; i++) {
+    cout << arry[i] << " ";
+  }
 
 }
