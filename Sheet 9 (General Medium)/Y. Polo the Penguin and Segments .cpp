51b9fc72585775491cce7b27ed3ffbcc7ff0422b
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-09-25 17:07:50 +0300

    Sheet 9 (General Medium)

--- Sheet 9 (General Medium)/Y. Polo the Penguin and Segments .cpp
+++ Sheet 9 (General Medium)/Y. Polo the Penguin and Segments .cpp
@@ -2,26 +2,26 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	long long size, number;
-	cin >> size >> number;
+  long long size, number;
+  cin >> size >> number;
 
-	long long cover = 0;
+  long long cover = 0;
 
-	for (int i = 0; i < size; i++) {
-		int first, second;
-		cin >> first >> second;
+  for (int i = 0; i < size; i++) {
+    int first, second;
+    cin >> first >> second;
 
-		cover += (second - first + 1);
-	}
+    cover += (second - first + 1);
+  }
 
-	cover %= number;
+  cover %= number;
 
-	if (cover == 0) {
-		cout << 0;
-	} else {
-		cout << number - cover;
-	}
+  if (cover == 0) {
+    cout << 0;
+  } else {
+    cout << number - cover;
+  }
 
 }
--- Sheet 9 (General Medium)/Z. Fraction .cpp
+++ Sheet 9 (General Medium)/Z. Fraction .cpp
@@ -1,30 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long long gcd (long long number_1, long long number_2) {
+long long gcd(long long number_1, long long number_2) {
 
-	while (number_2 != 0) {
-		return gcd(number_2, number_1 % number_2);
-	}
-
-	return number_1;
-
-}
-
-long long lcm (long long number_1, long long number_2) {
-
-	return ((number_1 / gcd (number_1, number_2) ) * number_2);
+  while (number_2 != 0) {
+    return gcd(number_2, number_1 % number_2);
+  }
+  return number_1;
 
 }
 
-int main(){
+long long lcm(long long number_1, long long number_2) {
 
-	long long number_1, number_2, number_3, number_4;
-	char slash_1, slash_2;
+  return (number_1 / gcd(number_1, number_2)) * number_2;
 
-	cin >> number_1 >> slash_1 >> number_2 >> number_3 >> slash_2 >> number_4;
+}
 
-	cout << lcm (number_1, number_3) << "/" << gcd (number_2, number_4);
+int main() {
+
+  long long number_1, number_2, number_3, number_4;
+  char slash_1, slash_2;
+
+  cin >> number_1 >> slash_1 >> number_2 >> number_3 >> slash_2 >> number_4;
+
+  long long numerator_lcm = lcm(number_1, number_3);
+  long long denominator_gcd = gcd(number_2, number_4);
+
+  cout << numerator_lcm << "/" << denominator_gcd << endl;
 
 }
