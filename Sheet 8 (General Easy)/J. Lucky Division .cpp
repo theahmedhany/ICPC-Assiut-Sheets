52a519ab762c6b216442cb4ecaff11444a6f9d8d
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-09-29 22:33:22 +0300

    Sheet 8 (General Easy)\

--- Sheet 8 (General Easy)/J. Lucky Division .cpp
+++ Sheet 8 (General Easy)/J. Lucky Division .cpp
@@ -2,31 +2,31 @@
 
 using namespace std;
 
-bool isLucky(int value){
+bool isLucky(int value) {
 
-	while (value > 0){
-		if (value % 10 != 7 and value % 10 != 4){
-			return false;
-		}
-		value /= 10;
-	}
-	
-	return true;
+  while (value > 0) {
+    int digit = value % 10;
+    if (digit != 7 && digit != 4) {
+      return false;
+    }
+    value /= 10;
+  }
+  return true;
 
 }
 
-int main(){
+int main() {
 
-	int number;
-	cin >> number;
+  int number;
+  cin >> number;
 
-	for (int i = 4; i <= number; i++){
-		if (isLucky(i) and number % i == 0){
-			cout << "YES" << endl;
-			return 0;
-		}
-	}
-	
-	cout << "NO" << endl;
+  for (int i = 4; i <= number; i++) {
+    if (isLucky(i) && number % i == 0) {
+      cout << "YES" << endl;
+      return 0;
+    }
+  }
+
+  cout << "NO" << endl;
 
 }
