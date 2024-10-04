b7610c8d0b06c64723806dbf42a946da24b9c889
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-04 11:47:18 +0300

    Sheet 8 (General Easy)

--- Sheet 8 (General Easy)/W. Mahmoud and Longest Uncommon Subsequence .cpp
+++ Sheet 8 (General Easy)/W. Mahmoud and Longest Uncommon Subsequence .cpp
@@ -4,17 +4,15 @@ using namespace std;
 
 int main() {
 
-	string txt_1, txt_2;
-	cin >> txt_1 >> txt_2;
+  string txt_1, txt_2;
+  cin >> txt_1 >> txt_2;
 
-	if (txt_1 == txt_2){
-		cout << "-1" << endl;
-	} else if (txt_1.size() > txt_2.size()) {
-		cout << txt_1.size() << endl;
-	} else {
-		cout << txt_2.size() << endl;
-	}
-	
-	
+  if (txt_1 == txt_2) {
+    cout << "-1" << endl;
+  } else if (txt_1.size() > txt_2.size()) {
+    cout << txt_1.size() << endl;
+  } else {
+    cout << txt_2.size() << endl;
+  }
 
 }
--- Sheet 8 (General Easy)/X. Panoramix's Prediction .cpp
+++ Sheet 8 (General Easy)/X. Panoramix's Prediction .cpp
@@ -2,51 +2,47 @@
 
 using namespace std;
 
-bool isPrime(int number){
+bool isPrime(int number) {
 
-	if(number == 0 || number == 1)
-		return false;
+  if (number <= 1)
+    return false;
 
-	for(int i = 2; i <= number / 2; i++){
-		if(number % i == 0){
-			return false;
-		}
-	}
-
-	return true;
-
-}
-
-int nextPrime(int number){
-
-	if(number <= 1){
-		return 2;
-	}
-
-	bool found = false;
-	int prime_number = number;
-
-	while(!found){
-		prime_number++;
-
-		if(isPrime(prime_number)){
-			found = true;
-		}
-	}
-
-	return prime_number;
+  for (int i = 2; i <= number / 2; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
 
 }
 
-int main(){
+int nextPrime(int number) {
 
-	int number_1, number_2;
-	cin >> number_1 >> number_2;
+  if (number <= 1)
+    return 2;
 
-	if(nextPrime(number_1) == number_2){
-		cout << "YES" << endl;
-	} else {
-		cout << "NO" << endl;
-	}
+  int prime_number = number;
+  bool found = false;
+
+  while (!found) {
+    prime_number++;
+    if (isPrime(prime_number)) {
+      found = true;
+    }
+  }
+  return prime_number;
+
+}
+
+int main() {
+
+  int number_1, number_2;
+  cin >> number_1 >> number_2;
+
+  if (nextPrime(number_1) == number_2) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
 
 }
