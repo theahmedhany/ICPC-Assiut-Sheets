1960a5b476f6e982e16feae7e5cfd03ade4c018e
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-10 22:26:15 +0300

    Sheet 7 (Recursion)

--- Sheet 7 (Recursion)/K. Max Number .cpp
+++ Sheet 7 (Recursion)/K. Max Number .cpp
@@ -5,24 +5,24 @@ using namespace std;
 int arry[1000];
 int arry_size;
 
-int getMaxNumber(int index){
+int getMaxNumber(int index) {
 
-	if(index == arry_size - 1){
-		return arry[index];
-	}
+  if (index == arry_size - 1) {
+    return arry[index];
+  }
 
-	return max(arry[index], getMaxNumber(index + 1));
+  return max(arry[index], getMaxNumber(index + 1));
 
 }
 
-int main(){
+int main() {
 
-	cin >> arry_size;
+  cin >> arry_size;
 
-	for(int i = 0; i < arry_size; i++){
-		cin >> arry[i];
-	}
+  for (int i = 0; i < arry_size; i++) {
+    cin >> arry[i];
+  }
 
-	cout << getMaxNumber(0) << endl;
+  cout << getMaxNumber(0) << endl;
 
 }
