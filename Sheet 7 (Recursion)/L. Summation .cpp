86e2b0f3968eb89ec6db9e159b5ac144a706c2ae
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-11 11:48:20 +0300

    Sheet 7 (Recursion)

--- Sheet 7 (Recursion)/L. Summation .cpp
+++ Sheet 7 (Recursion)/L. Summation .cpp
@@ -5,25 +5,24 @@ using namespace std;
 int arry[1000];
 int arry_size;
 
+long long getSummation(int index) {
 
-long long getSummation(int index){
+  if (index == arry_size - 1) {
+    return arry[index];
+  }
 
-	if(index == arry_size - 1){
-		return arry[index];
-	}
-
-	return arry[index] + getSummation(index + 1);
+  return arry[index] + getSummation(index + 1);
 
 }
 
-int main(){
+int main() {
 
-	cin >> arry_size;
+  cin >> arry_size;
 
-	for(int i = 0; i < arry_size; i++){
-		cin >> arry[i];
-	}
+  for (int i = 0; i < arry_size; i++) {
+    cin >> arry[i];
+  }
 
-	cout << getSummation(0) << endl;
+  cout << getSummation(0) << endl;
 
 }
