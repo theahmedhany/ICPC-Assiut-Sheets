f782aebcd0adde2bfcdf12db85fa32e46e67f02f
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-13 12:47:36 +0300

    Sheet 7 (Recursion)

--- Sheet 7 (Recursion)/S. Array Average .cpp
+++ Sheet 7 (Recursion)/S. Array Average .cpp
@@ -5,24 +5,24 @@ using namespace std;
 double arry[100];
 int arry_size;
 
-double arrayAverage(int index, double result){
+double arrayAverage(int index, double result) {
 
-	if(arry_size == index){
-		return result / arry_size;
-	}
+  if (index == arry_size) {
+    return result / arry_size;
+  }
 
-	return arrayAverage(index + 1, result + arry[index]);
+  return arrayAverage(index + 1, result + arry[index]);
 
 }
 
-int main(){
+int main() {
 
-	cin >> arry_size;
+  cin >> arry_size;
 
-	for(int i = 0; i < arry_size; i++){
-		cin >> arry[i];
-	}
+  for (int i = 0; i < arry_size; i++) {
+    cin >> arry[i];
+  }
 
-	cout << fixed << setprecision(6) << arrayAverage(0 , 0);
+  cout << fixed << setprecision(6) << arrayAverage(0, 0) << endl;
 
 }
--- Sheet 7 (Recursion)/T. Combination .cpp
+++ Sheet 7 (Recursion)/T. Combination .cpp
@@ -2,32 +2,32 @@
 
 using namespace std;
 
-long long Combination(long long number_1, long long number_2){
+long long Combination(long long number_1, long long number_2) {
 
-	long long result = 1;
-	long long temp = 2;
+  long long result = 1;
+  long long temp = 2;
 
-	for(int i = number_2 + 1; i <= number_1; i++){
-		result *= i;
-		if(temp <= number_1 - number_2 && result % temp == 0){
-			result /= temp;
-			temp++;
-		}
-	}
+  for (int i = number_2 + 1; i <= number_1; i++) {
+    result *= i;
+    if (temp <= number_1 - number_2 && result % temp == 0) {
+      result /= temp;
+      temp++;
+    }
+  }
 
-	return result;
+  return result;
 
 }
 
-int main(){
+int main() {
 
-	int number_1, number_2;
-	cin >> number_1 >> number_2;
+  long long number_1, number_2;
+  cin >> number_1 >> number_2;
 
-	if(number_2 > number_1){
-		cout << 0 << endl;
-	} else {
-		cout << Combination(number_1, number_2);
-	}
+  if (number_2 > number_1) {
+    cout << 0 << endl;
+  } else {
+    cout << Combination(number_1, number_2) << endl;
+  }
 
 }
