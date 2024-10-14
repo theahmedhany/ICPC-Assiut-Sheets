b418642078dc14ea7f5592ac30380d949c19d58f
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-14 13:31:39 +0300

    Sheet 7 (Recursion)

--- Sheet 7 (Recursion)/U. Knapsack .cpp
+++ Sheet 7 (Recursion)/U. Knapsack .cpp
@@ -2,35 +2,35 @@
 
 using namespace std;
 
-int Knapsack(int items_weight, int weight[], int value [], int items){
+int Knapsack(int items_weight, int weight[], int value[], int items) {
 
-	if(items_weight == 0 || items == 0){
-		return 0;
-	}
+  if (items_weight == 0 || items == 0) {
+    return 0;
+  }
 
-	if(weight[items - 1] > items_weight){
-		return Knapsack(items_weight, weight, value, items - 1);
-	} else {
-		return max(
-			value[items - 1] + Knapsack(items_weight - weight[items - 1], weight, value, items - 1),
-			Knapsack(items_weight, weight, value, items - 1)
-		);
-	}
+  if (weight[items - 1] > items_weight) {
+    return Knapsack(items_weight, weight, value, items - 1);
+  } else {
+    return max(
+      value[items - 1] + Knapsack(items_weight - weight[items - 1], weight, value, items - 1),
+      Knapsack(items_weight, weight, value, items - 1)
+    );
+  }
 
 }
 
-int main(){
+int main() {
 
-	int items, items_weight;
-	cin >> items >> items_weight;
+  int items, items_weight;
+  cin >> items >> items_weight;
 
-	int weight [items];
-	int value [items];
+  int weight[items];
+  int value[items];
 
-	for(int i = 0; i < items; i++){
-		cin >> weight[i] >> value[i];
-	}
+  for (int i = 0; i < items; i++) {
+    cin >> weight[i] >> value[i];
+  }
 
-	cout << Knapsack (items_weight, weight, value, items) << endl;
+  cout << Knapsack(items_weight, weight, value, items) << endl;
 
 }
--- Sheet 7 (Recursion)/V. Creating Expression1 .cpp
+++ Sheet 7 (Recursion)/V. Creating Expression1 .cpp
@@ -5,31 +5,31 @@ using namespace std;
 long long elements, result;
 long long arry[21];
 
-bool createExpression(long long summation, long index){
+bool createExpression(long long summation, long index) {
 
-	if(index == elements){
-		return summation == result;
-	}
+  if (index == elements) {
+    return summation == result;
+  }
 
-	bool roll_1 = createExpression(summation + arry[index], index + 1);
-	bool roll_2 = createExpression(summation - arry[index], index + 1);
+  bool roll_1 = createExpression(summation + arry[index], index + 1);
+  bool roll_2 = createExpression(summation - arry[index], index + 1);
 
-	return roll_1 || roll_2;
+  return roll_1 || roll_2;
 
 }
 
 int main() {
 
-	cin >> elements >> result;
+  cin >> elements >> result;
 
-	for(int i = 0; i < elements; i++){
-		cin >> arry[i];
-	}
+  for (int i = 0; i < elements; i++) {
+    cin >> arry[i];
+  }
 
-	if(createExpression(arry[0], 1)){
-		cout << "YES" << endl;
-	} else {
-		cout << "NO" << endl;
-	}
+  if (createExpression(arry[0], 1)) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
 
 }
