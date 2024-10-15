19be3336961c87a59451e5dd709a954c377b388c
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-15 13:12:14 +0300

    Sheet 7 (Recursion)

--- Sheet 7 (Recursion)/W. Reach Value .cpp
+++ Sheet 7 (Recursion)/W. Reach Value .cpp
@@ -1,34 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 long long input;
 
-bool reachValue(long long number){
+bool reachValue(long long number) {
 
-	if(input < number){
-		return false;
-	} else if (input == number){
-		return true;
-	} else {
-		return reachValue(number * 10) || reachValue(number * 20);
-	}
+  if (number > input) {
+    return false;
+  }
+
+  if (number == input) {
+    return true;
+  }
+
+  return reachValue(number * 10) || reachValue(number * 20);
 
 }
 
-int main(){
+int main() {
 
-	long long cases;
-	cin >> cases;
+  long long cases;
+  cin >> cases;
 
-	while(cases--){
-		cin >> input;
+  while (cases--) {
+    cin >> input;
 
-		if(reachValue(1)){
-			cout << "YES" << endl;
-		} else {
-			cout << "NO" << endl;
-		}
-	}
+    if (reachValue(1)) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
+  }
 
 }
--- Sheet 7 (Recursion)/X. The maximum path-sum .cpp
+++ Sheet 7 (Recursion)/X. The maximum path-sum .cpp
@@ -5,32 +5,31 @@ using namespace std;
 int row, column;
 int arry[11][11];
 
-int maximumPathSum(int start, int end){
+int maximumPathSum(int start, int end) {
 
-	if(start == row - 1 && end == column - 1){
-		return arry[start][end];
-	} else if(start == row + 1 || end == column + 1){
-		return -1000000;
-	}
+  if (start == row - 1 && end == column - 1) {
+    return arry[start][end];
+  } else if (start == row + 1 || end == column + 1) {
+    return -1000000;
+  }
 
-	int right = maximumPathSum(start, end + 1);
-	int left = maximumPathSum(start + 1, end);
+  int right = maximumPathSum(start, end + 1);
+  int left = maximumPathSum(start + 1, end);
 
-	return arry[start][end] + max(right, left);
+  return arry[start][end] + max(right, left);
 
 }
 
 int main() {
 
-	cin >> row >> column;
+  cin >> row >> column;
 
-	for(int i = 0; i < row; i++){
-		for(int z = 0; z < column; z++){
-			cin >> arry[i][z];
-		}
-	}
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < column; j++) {
+      cin >> arry[i][j];
+    }
+  }
 
-	cout << maximumPathSum(0, 0);
+  cout << maximumPathSum(0, 0) << endl;
 
 }
-
