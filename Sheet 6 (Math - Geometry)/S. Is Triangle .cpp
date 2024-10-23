4a72411d389f4e7a6e359952ae03fdbaed905c34
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-23 12:18:05 +0300

    Sheet 6 (Math - Geometry)

--- Sheet 6 (Math - Geometry)/S. Is Triangle .cpp
+++ Sheet 6 (Math - Geometry)/S. Is Triangle .cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main() {
 
-	double number_1, number_2, number_3;
-	cin >> number_1 >> number_2 >> number_3;
+  double number_1, number_2, number_3;
+  cin >> number_1 >> number_2 >> number_3;
 
-	if(number_1 + number_2 > number_3 && number_1 + number_3 > number_2 && number_2 + number_3 > number_1){
-		double sum = number_1 + number_2 + number_3;
-		
-		cout << "Valid" << endl;
-		cout << fixed << setprecision(6) << sqrt((sum / 2) * (sum / 2 - number_1) * (sum / 2 - number_2) * (sum / 2 - number_3));
-	} else {
-		cout << "Invalid" << endl;
-	}
+  if (number_1 + number_2 > number_3 && number_1 + number_3 > number_2 && number_2 + number_3 > number_1) {
+    double semi_perimeter = (number_1 + number_2 + number_3) / 2;
+    double area = sqrt(semi_perimeter * (semi_perimeter - number_1) * (semi_perimeter - number_2) * (semi_perimeter - number_3));
+
+    cout << "Valid" << endl;
+    cout << fixed << setprecision(6) << area << endl;
+  } else {
+    cout << "Invalid" << endl;
+  }
 
 }
--- Sheet 6 (Math - Geometry)/T. Circle Task .cpp
+++ Sheet 6 (Math - Geometry)/T. Circle Task .cpp
@@ -4,27 +4,26 @@ using namespace std;
 
 int main() {
 
-	long long x_axes[1000];
-	long long y_axes[1000];
+  long long x_coordinate, y_coordinate, radius, points;
+  cin >> x_coordinate >> y_coordinate >> radius >> points;
 
-	long long x_coordinate, y_coordinate, radius, points;
-	cin >> x_coordinate >> y_coordinate >> radius >> points;
+  long long x_axes[1000];
+  long long y_axes[1000];
 
-	for(int i = 0; i < points; i++){
-		cin >> x_axes[i] >> y_axes[i];
-	}
-	
-	for(int i = 0; i < points; i++){
-		long long x1 = x_coordinate - x_axes[i];
-		long long y1 = y_coordinate - y_axes[i];
+  for (int i = 0; i < points; i++) {
+    cin >> x_axes[i] >> y_axes[i];
+  }
 
-		double result = sqrt(pow(x1 , 2) + pow(y1 , 2));
+  for (int i = 0; i < points; i++) {
+    long long dx = x_coordinate - x_axes[i];
+    long long dy = y_coordinate - y_axes[i];
+    double distance = sqrt(pow(dx, 2) + pow(dy, 2));
 
-		if(result > radius){
-			cout << "NO" << endl;
-		} else {
-			cout << "YES" << endl;
-		}
-	}
+    if (distance > radius) {
+      cout << "NO" << endl;
+    } else {
+      cout << "YES" << endl;
+    }
+  }
 
 }
