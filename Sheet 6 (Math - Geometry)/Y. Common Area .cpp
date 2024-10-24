d3bff224a02ff61238ad159786d3351aff9a0d01
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-24 19:34:52 +0300

    Sheet 6 (Math - Geometry)

--- Sheet 6 (Math - Geometry)/Y. Common Area .cpp
+++ Sheet 6 (Math - Geometry)/Y. Common Area .cpp
@@ -7,42 +7,41 @@ int main() {
   long long test_cases;
   cin >> test_cases;
 
-  int counter = 1;
+  int case_counter = 1;
 
-  while(counter <= test_cases){
+  while (case_counter <= test_cases) {
+    long long rectangles_number;
+    cin >> rectangles_number;
 
-		long long number;
-		cin >> number;
+    long long bottom_left_x, bottom_left_y, top_right_x, top_right_y;
+    cin >> bottom_left_x >> bottom_left_y >> top_right_x >> top_right_y;
 
-		long long point_1_x, point_1_y, point_2_x, point_2_y;
-		cin >> point_1_x >> point_1_y >> point_2_x >> point_2_y;
+    long long max_bottom_left_x = bottom_left_x;
+    long long max_bottom_left_y = bottom_left_y;
 
-		long long max_1_x = point_1_x;
-		long long max_1_y = point_1_y;
+    long long min_top_right_x = top_right_x;
+    long long min_top_right_y = top_right_y;
 
-		long long min_x_2 = point_2_x;
-		long long min_y_2 = point_2_y;
+    for (int i = 1; i < rectangles_number; i++) {
+      cin >> bottom_left_x >> bottom_left_y >> top_right_x >> top_right_y;
 
-		for(int i = 1; i < number; i++){
+      max_bottom_left_x = max(max_bottom_left_x, bottom_left_x);
+      max_bottom_left_y = max(max_bottom_left_y, bottom_left_y);
 
-			cin >> point_1_x >> point_1_y >> point_2_x >> point_2_y;
+      min_top_right_x = min(min_top_right_x, top_right_x);
+      min_top_right_y = min(min_top_right_y, top_right_y);
+    }
 
-			max_1_x = max(max_1_x, point_1_x);
-			max_1_y = max(max_1_y, point_1_y);
-			
-			min_x_2 = min(min_x_2, point_2_x);
-			min_y_2 = min(min_y_2, point_2_y);
+    if (min_top_right_x > max_bottom_left_x && min_top_right_y > max_bottom_left_y) {
+      long long width = min_top_right_x - max_bottom_left_x;
+      long long height = min_top_right_y - max_bottom_left_y;
 
-		}
+      cout << "Case #" << case_counter << ": " << width * height << endl;
+    } else {
+      cout << "Case #" << case_counter << ": 0" << endl;
+    }
 
-		if(min_x_2 > max_1_x && min_y_2 > max_1_y) {
-			cout << "Case #" << counter << ": " << (min_x_2 - max_1_x) * (min_y_2 - max_1_y) << endl;
-		} else {
-			cout << "Case #" << counter << ": " << 0 << endl;
-		}
-
-		counter++;
-
-	}
+    case_counter++;
+  }
 
 }
--- Sheet 6 (Math - Geometry)/Z. Geometry Test .cpp
+++ Sheet 6 (Math - Geometry)/Z. Geometry Test .cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main() {
 
-	long long radius, side;
-	cin >> radius >> side;
+  long long radius, side;
+  cin >> radius >> side;
 
-	if(side >= radius * 2){
-		cout << "Square" << endl;
-	} else if (radius * 2 > sqrt(pow(side, 2) + pow(side, 2))){
-		cout << "Circle" << endl;
-	} else {
-		cout << "Complex" << endl;
-	}
+  if (side >= radius * 2) {
+    cout << "Square" << endl;
+  } else if (radius * 2 > sqrt(pow(side, 2) + pow(side, 2))) {
+    cout << "Circle" << endl;
+  } else {
+    cout << "Complex" << endl;
+  }
 
 }
